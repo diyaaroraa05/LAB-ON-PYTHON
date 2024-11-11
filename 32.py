e0cf504a07a6fee1b2685a48be8bb4cfbc1febdc
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:18:09 +0530

    Update 32.py

--- 32.py
+++ 32.py
@@ -88,4 +88,4 @@ for i in range(n - 1, 0, -1):
     for k in range(2 * i - 1):
         print('*', end='')
     print()
-    print("This code is written by AARYAN ERP- 0221BCA163")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
