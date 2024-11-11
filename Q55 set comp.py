2e48191bc4972de865901e866ad808cad36766b7
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:39:05 +0530

    Update Q55 set comp.py

--- Q55 set comp.py
+++ Q55 set comp.py
@@ -1,4 +1,4 @@
 #Program to Use Set Comprehension:
 squares = {x**2 for x in range(5)}
 print(squares) # Outputs: {0, 1, 4, 9, 16}
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
