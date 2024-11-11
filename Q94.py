1199a03627c4c56ed8ea0be518f0c52d8ef20e0b
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:29:15 +0530

    Update Q94.py

--- Q94.py
+++ Q94.py
@@ -2,4 +2,4 @@ import importlib
 math_utils = importlib.import_module('math_utils')
 result = math_utils.add(5, 3)
 print(result)
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
