83299d57f0421700d690e842aaa219e1017ac89b
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:16:12 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -6,4 +6,4 @@ print(f"Square root of {num} is {math.sqrt(num)}")
 print(f"Factorial of {num} is {math.factorial(num)}")
 print(f"Logarithm of {num} is {math.log2(num)}")
 
-print("This code is written by AARYAN ERP- 0221BCA163")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
