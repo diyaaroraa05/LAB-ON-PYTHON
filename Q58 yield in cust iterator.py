8f69d886c9e382d8afdaafeac1ab514df06bd493
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:38:11 +0530

    Update Q58 yield in iterator.py

--- Q58 yield in cust iterator.py
+++ Q58 yield in cust iterator.py
@@ -9,4 +9,4 @@ class Countdown:
         self.n -= 1
 for number in Countdown(5):
  print(number) # Outputs: 5, 4, 3, 2, 1
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
