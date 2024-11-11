bc44201aff39be8e4917da4648787600edf2cd2a
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:39:34 +0530

    Update Q53 chain generator.py

--- Q53 chain generator.py
+++ Q53 chain generator.py
@@ -9,4 +9,4 @@ def squared_numbers(numbers):
   yield number ** 2
 for square in squared_numbers(count_up_to(5)):
  print(square) # Outputs: 1, 4, 9, 16, 25
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
