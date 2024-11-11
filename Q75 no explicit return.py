19beac2083bcdad70391f86f441bc3883c302c7b
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:32:34 +0530

    Update Q75 no explicit return.py

--- Q75 no explicit return.py
+++ Q75 no explicit return.py
@@ -4,4 +4,4 @@ def greet(name):
  print(f"Hello, {name}!")
 result = greet("Alice")
 print(result) # Output: None
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
