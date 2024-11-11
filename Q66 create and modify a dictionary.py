a5d3688e7920bdedfb7948f04c0ea5dd9b56964a
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:35:45 +0530

    Update Q66 create and modify a dictionary.py

--- Q66 create and modify a dictionary.py
+++ Q66 create and modify a dictionary.py
@@ -3,4 +3,4 @@ my_dict = {'name': 'Alice', 'age': 25}
 my_dict['age'] = 26
 my_dict['city'] = 'New York'
 print(my_dict) # Outputs: {'name': 'Alice', 'age': 26, 'city': 'New York'}
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
