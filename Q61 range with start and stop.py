218477bf1dca5fc3980e3fa707528d8730c302bb
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:37:23 +0530

    Update Q61 range with start and stop.py

--- Q61 range with start and stop.py
+++ Q61 range with start and stop.py
@@ -1,4 +1,4 @@
 #Program to Use range() with Start and Stop:
 for i in range(2, 6):
  print(i) # Outputs: 2, 3, 4, 5
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
