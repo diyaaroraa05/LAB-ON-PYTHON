0f8e1ed6b681274ae521fcfc55feec6317f87c81
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:37:33 +0530

    Update Q60 range for iteration.py

--- Q60 range for iteration.py
+++ Q60 range for iteration.py
@@ -1,4 +1,4 @@
 #Program to Use range() for Iteration:
 for i in range(5):
  print(i) # Outputs: 0, 1, 2, 3, 4
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
