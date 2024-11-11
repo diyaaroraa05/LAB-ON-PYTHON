b9c0e6ec8a856142489c10e99ed24ecf283f7c9b
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:40:30 +0530

    Update Q48 list comprehensions.py

--- Q48 list comprehensions.py
+++ Q48 list comprehensions.py
@@ -3,7 +3,7 @@ squares = [x**2 for x in range(10)]
 even_squares = [x**2 for x in range(10) if x % 2 == 0]
 print("Squares:", squares)
 print("Even squares:", even_squares)
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
 
 
 
