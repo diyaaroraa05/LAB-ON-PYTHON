bdcad250f9231a512ff58c6ef79550c4ef7285ad
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:40:20 +0530

    Update Q49 built in iterator.py

--- Q49 built in iterator.py
+++ Q49 built in iterator.py
@@ -3,6 +3,6 @@ my_list = [1, 2, 3, 4]
 it = iter(my_list)
 print(next(it)) # Outputs: 1
 print(next(it)) # Outputs: 2
-print("Program by jashn sharma - 0221BCA048")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
 
 
