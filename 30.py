e3afd23c9e79ae878a2a8668c56d636a41eb375b
diyaaroraa05 <diyaaroraa18@gmail.com>
2024-11-11 11:17:41 +0530

    Update 30.py

--- 30.py
+++ 30.py
@@ -4,4 +4,4 @@ while count < 5:
     print("Count is:", count)
     count += 1
 
-    print("This code is written by AARYAN ERP- 0221BCA163")
+print("This code is written by DIYA ARORA ERP-0221BCA059")
